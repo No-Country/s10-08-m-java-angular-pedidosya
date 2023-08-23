ec687e4fa3f0aaf3deb9e01b9e4a6dee68384018
npotaves <104572840+npotaves@users.noreply.github.com>
2023-08-23 01:15:54 -0300

    add-model-layer

---
+++ api/src/main/java/com/pedidosya/api/models/City.java
@@ -0,0 +1,24 @@
+package com.pedidosya.api.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class City {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCity;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "id_province", nullable = false, foreignKey = @ForeignKey(name= "FK_City_Province"))
+    private Country province;
+}
---
+++ api/src/main/java/com/pedidosya/api/models/Product.java
@@ -0,0 +1,42 @@
+package com.pedidosya.api.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Product {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idProduct;
+
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String description;
+
+    @Column(columnDefinition = "decimal(18,4)" , nullable = false)
+    private double price;
+    @Column(length = 300, nullable = false)
+    private String imagePath;
+
+    @ManyToOne
+    @JoinColumn(name = "id_product_type", nullable = false, foreignKey = @ForeignKey(name= "FK_Product_Product_Type"))
+    private ProductType productType;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "id_menu", nullable = false, foreignKey = @ForeignKey(name= "FK_Product_Menu"))
+    private Menu menu;
+
+
+}
