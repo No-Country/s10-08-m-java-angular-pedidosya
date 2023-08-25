d53abdd875f922dded173632f3519986b21ae5e1
Javier <javoalon@gmail.com>
2023-08-25 20:25:41 -0300

    Add home page, components for restaurant-card, and carousel of restaurants

--- dev-front/src/app/app.module.ts
+++ dev-front/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { RestaurantCardComponent } from './components/restaurant-card/restaurant-card.component';
+import { CarouselRestaurantsComponent } from './components/carousel-restaurants/carousel-restaurants.component';
+import { RestaurantCardForCarouselComponent } from './components/restaurant-card-for-carousel/restaurant-card-for-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    HomeComponent,
+    RestaurantCardComponent,
+    CarouselRestaurantsComponent,
+    RestaurantCardForCarouselComponent
   ],
   imports: [
     BrowserModule,
---
+++ dev-front/src/app/components/restaurant-card/restaurant-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-card',
+  templateUrl: './restaurant-card.component.html',
+  styleUrls: ['./restaurant-card.component.scss']
+})
+export class RestaurantCardComponent {
+  @Input() image: string ='';
+  @Input() price: string = '';
+}
