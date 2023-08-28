5d37a5001750bb3b94226cc10924ee020e1a4310
Johana Carrizo <johanacarrizo20@gmail.com>
2023-08-28 17:33:14 -0300

    feat:added endpoint getAll client

--- api/src/main/java/com/pedidosya/api/utils/mappers/IClientMapper.java
+++ api/src/main/java/com/pedidosya/api/utils/mappers/IClientMapper.java
@@ -3,6 +3,9 @@ package com.pedidosya.api.utils.mappers;
 import com.pedidosya.api.dto.Request.ClientDTO;
 import com.pedidosya.api.models.Client;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface IClientMapper {
