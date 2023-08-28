e88adbf546e23520f22868ce30ee70b4f7dd83a5
Javier <javoalon@gmail.com>
2023-08-28 19:06:24 -0300

    Add page to activate ubication of user. Just style, no interaction

--- dev-front/src/app/app-routing.module.ts
+++ dev-front/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from "@root/components/home/home.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "@root/components/home/home.component";
 import { LoginUserComponent } from './login-user/login-user.component';
+import { UbiUserComponent } from './ubi-user/ubi-user.component';
 
 const routes: Routes = [
   // {path: 'inicio', component: InicioComponent, canActivate: [SesionGuard]},
-  {path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((modulo) => modulo.AuthModule)
@@ -14,8 +15,9 @@ const routes: Routes = [
     path: 'restaurant',
     loadChildren: () => import('./restaurant/restaurant.module').then(mod => mod.RestaurantModule)
   },
-  {path: 'onboarding' , component: LoginUserComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'onboarding', component: LoginUserComponent },
+  { path: 'onboarding-ubication', component: UbiUserComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   // {path: '**', component: NoEncontradoComponent },
 ];
 
