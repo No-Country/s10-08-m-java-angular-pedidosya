7f624041a8f3d0e36fd95f4360ca647654f8aa5e
npotaves <104572840+npotaves@users.noreply.github.com>
2023-09-14 01:31:50 -0300

    update

--- api/src/main/java/com/pedidosya/api/utils/mappers/ISaleResponseMapper.java
+++ api/src/main/java/com/pedidosya/api/utils/mappers/ISaleResponseMapper.java
@@ -9,13 +9,13 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", uses = { IUserMapper.class, IStoreMapper.class, IDeliveryMapper.class, IAddressMapper.class })
 public interface ISaleResponseMapper {
      @Mapping(source = "user.idUser", target = "idUser")
-    @Mapping(source = "store.idStore", target = "idStore")
+    //@Mapping(source = "store.idStore", target = "idStore")
     @Mapping(source = "delivery.idDelivery", target = "idDelivery")
-     @Mapping(source = "address.idAddress", target = "idAddress")
+    // @Mapping(source = "address.idAddress", target = "idAddress")
     SaleResponseDTO toSaleDto(Sale sale);
     @Mapping(source = "idUser", target = "user.idUser")
-    @Mapping(source = "idStore", target = "store.idStore")
+    //@Mapping(source = "idStore", target = "store.idStore")
     @Mapping(source = "idDelivery", target = "delivery.idDelivery")
-    @Mapping(source = "idAddress", target = "address.idAddress")
+    //@Mapping(source = "idAddress", target = "address.idAddress")
     Sale toSale(SaleResponseDTO saleDTO);
 }
