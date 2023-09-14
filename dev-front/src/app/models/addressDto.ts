125809dd145d4e5922e5a58e5f0a699d3a777345
Agustin Rios <agustin528rios@gmail.com>
2023-09-14 02:08:18 -0300

    address service and model

---
+++ dev-front/src/app/models/addressDto.ts
@@ -0,0 +1,25 @@
+export interface AddressDTO {
+    idClient: number,
+    address: {
+        idAddress: number,
+        description: string,
+        latitude: number,
+        longitude: number,
+        idCity: number
+    },
+    set: boolean
+}
+
+export interface AddressGet {
+    idClient: number
+}
+
+export interface Address {
+    address: {
+        idAddress: number,
+        description: string,
+        latitude: DoubleRange,
+        longitude: DoubleRange,
+        idCity: number
+    },
+}
