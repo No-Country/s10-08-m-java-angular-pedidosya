bf3aced09d4040aeec64305f26d085458fd2d43c
Agustin Rios <agustin528rios@gmail.com>
2023-09-15 20:11:44 -0300

    add back-button

--- dev-front/src/app/components/restaurant-types/restaurant-types.component.ts
+++ dev-front/src/app/components/restaurant-types/restaurant-types.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-restaurant-types',
   templateUrl: './restaurant-types.component.html',
-  styleUrls: ['./restaurant-types.component.scss'],
+  styleUrls: ['./restaurant-types.component.scss','../../customer/profile-root/profile-root.scss'],
 })
 export class RestaurantTypesComponent {
 }
