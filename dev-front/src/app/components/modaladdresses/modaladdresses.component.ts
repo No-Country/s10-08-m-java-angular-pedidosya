7b8efb48f03b17ccff545554c63b9c380d721130
Agustin Rios <agustin528rios@gmail.com>
2023-09-15 20:15:20 -0300

    add with the api

--- dev-front/src/app/components/modaladdresses/modaladdresses.component.ts
+++ dev-front/src/app/components/modaladdresses/modaladdresses.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { AddressDTO, AddressGet } from '@models/addressDto';
+import { AddressService } from '@services/address.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-modaladdresses',
   templateUrl: './modaladdresses.component.html',
   styleUrls: ['./modaladdresses.component.scss']
 })
-export class ModaladdressesComponent {
+export class ModaladdressesComponent implements OnInit {
   mostrarMenu: boolean = true;
+  addresses: AddressDTO[] = [];
+
+  constructor(private addressService: AddressService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.getAddresses().subscribe((data) => {
+      this.addresses = data;
+      console.log(this.addresses);
+    });
+  }
+
+  getAddresses(): Observable<AddressDTO[]> {
+    return this.addressService.getListOfAddresses();
+  }
+
+  setAddress(addressDto:AddressDTO){
+    this.addressService.setAddress(addressDto).subscribe(
+      (response) => {
+        console.log("La solicitud PUT se completó con éxito", response);
+        this.getAddresses().subscribe((data) => {
+          this.addresses = data;
+          console.log(this.addresses);
+          this.cdr.detectChanges();
+        });
+      },
+      (error) => {
+        console.error("Error al realizar la solicitud PUT", error);
+      }
+    );
+    this.toggleDireccionMenu();
+
+  }
 
   toggleDireccionMenu() {
     this.mostrarMenu = !this.mostrarMenu;
